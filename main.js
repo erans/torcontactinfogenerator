51764884809e87f89ec6e31e570296e5f34af492
Eran Sandler <eran@sandler.co.il>
2018-07-13 16:25:56 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+$(document).ready( function() {
+  $("#generate-button").click(function(event) {
+    var result = new Array();
+    event.preventDefault();
+
+    var inputs = $("input");
+    var inputsLength = inputs.length;
+    for (var i = 0; i < inputsLength; i++) {
+      var element = $(inputs[i]);
+      var name = element.attr("id")
+      var type = element.attr("type");
+
+      if (type == "checkbox") {
+        if (element.prop("checked")) {
+          result.push(name + ":y");
+        } else {
+          //value = "n";
+        }
+      } else {
+        var value = element.val();
+        if (name == "email") {
+          value = value.replace("@", "[]");
+        }
+
+        if (value != "") {
+          result.push(name + ":" + value);
+        }
+      }
+
+
+    }
+
+    $("#result").val(result.join(" "));
+  })
+});
