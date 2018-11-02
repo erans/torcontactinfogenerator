f8b43d2e195e0a00be090a83eb8cd934c1c40acd
Eran Sandler <eran.sandler@gmail.com>
2018-11-02 10:53:27 -0700

    added ContactInfo in front of the generated text

--- main.js
+++ main.js
@@ -50,6 +50,7 @@ $(document).ready( function() {
 
   $("#generate-button").click(function(event) {
     var result = new Array();
+    result.push("ContactInfo");
     event.preventDefault();
 
     var inputs = $("input");
